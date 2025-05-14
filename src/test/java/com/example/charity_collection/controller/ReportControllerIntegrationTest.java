331671adb4a140b501b87e9d54bbeb71a34547f4
BartoszMordarski <bartosz.mordarski@gmail.com>
2025-05-14 19:53:10 +0200

    integration tests fix

--- src/test/java/com/example/charity_collection/controller/ReportControllerIntegrationTest.java
+++ src/test/java/com/example/charity_collection/controller/ReportControllerIntegrationTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ReportControllerIntegrationTest {
 
     @Autowired
